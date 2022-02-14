902cabe52f3a0fb422bd0d8f2ac411fe70b380ed
yujin <84204420+euphorxa@users.noreply.github.com>
2022-02-13 22:18:16 -0800

    Added more dependencies because I went turbo lazy zzzzz

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,12 @@
 object Dependencies {
+    // Required that we update this to the latest
+    const val JVM_TARGET = "17"
     const val KOTLIN = "1.6.10"
+
+    // Other dependencies
     const val FABRIC_LOOM = "0.11-SNAPSHOT"
     const val PREPROCESSOR = "0ab22d2"
+    const val KTOR = "1.6.7"
+    const val KOTLINX_SERIALIZATION = "1.3.2"
+    const val KOTLINX_COROUTINES = "1.6.0"
 }
